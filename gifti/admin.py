d45db9399839fd21254ee0cae7a96ddc356e9c47
Chrissy Fraser <plum5152@gmail.com>
2023-03-18 21:27:06 -0700

    Registered models in admin

--- gifti/admin.py
+++ gifti/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Wish, Brand, Store 
 
 # Register your models here.
+admin.site.register(Wish)
+admin.site.register(Brand)
+admin.site.register(Store)
