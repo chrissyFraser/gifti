0d1fd8d9ecf57b1b3c8775247bd682090b381981
Chrissy Fraser <plum5152@gmail.com>
2023-03-18 21:22:31 -0700

    Wish, Store, & Brand models

--- gifti/models.py
+++ gifti/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+
+class BrandName(models.Model):
+    brand_name = models.CharField(max_length=100)
+
+
+class StoreName(models.Model):
+    store_name = models.CharField(max_length=100)
+
+
+class Wish(models.Model):
+    item = models.CharField(max_length=200) 
+    brand = models.ForeignKey(BrandName, related_name="wish", on_delete=models.PROTECT)
+    picture = models.URLField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    store = models.ManyToManyField(StoreName, related_name="wish")
+    color = models.CharField(max_length=100, null=True, blank=True)
+    style = models.CharField(max_length=100, null=True, blank=True)
+    size = models.CharField(max_length=100, null=True, blank=True)
+    notes = models.TextField(max_length=100, null=True, blank=True)
+    bought = models.BooleanField()
+
+    def __str__(self):
+        return self.brand + self.item
+
